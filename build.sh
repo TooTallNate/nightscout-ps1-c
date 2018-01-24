f2e65fd6b33965d93672c3f09ae4803b80ccbc7c
Nathan Rajlich <nathan@tootallnate.net>
2018-01-23 23:26:53 -0800

    Attempt to run `make clean` inside the Docker image

--- build.sh
+++ build.sh
@@ -7,11 +7,9 @@ docker build -t nightscout-ps1-build:alpine -f Dockerfile.alpine .
 docker run -it --rm -v $PWD:/nightscout-ps1 nightscout-ps1-build:alpine
 mv -v packed/nightscout-ps1 "packed/nightscout-ps1-v${version}-alpine-x64"
 
-make clean
 docker build -t nightscout-ps1-build:debian -f Dockerfile.debian .
 docker run -it --rm -v $PWD:/nightscout-ps1 nightscout-ps1-build:debian
 mv -v packed/nightscout-ps1 "packed/nightscout-ps1-v${version}-linux-x64"
 
-make clean
 make build
 mv -v out/Default/nightscout-ps1 "packed/nightscout-ps1-v${version}-macos-x64"
